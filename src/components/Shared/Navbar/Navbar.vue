0e01926aeaa23db70b5e153dd69671a8f3a6a790
Mateusz <mateuszslotwinski16@gmail.com>
2022-01-07 00:24:53 +0100

    simple nav

--- src/components/Shared/Navbar/Navbar.vue
+++ src/components/Shared/Navbar/Navbar.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <nav>
+    <div class="logo">
+      <div class="icon" />
+      <div class="analyse">Analyse</div>
+      <div class="me">Me</div>
+    </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -10,4 +16,30 @@ export default defineComponent({})
 
 <style lang="scss" scoped>
 @import '@/styles/index.scss';
+nav {
+  width: 1600px;
+  max-width: 95vw;
+  background: $light;
+  margin: 10px auto;
+  padding: 10px;
+  border-radius: 7px;
+}
+
+.logo {
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+  font-weight: 700;
+  .icon {
+    height: 40px;
+    width: 40px;
+    background-image: url('../../../assets/logo/logo.png');
+    background-size: auto 100%;
+    background-repeat: no-repeat;
+    background-position-x: center;
+  }
+  .me {
+    color: $main;
+  }
+}
 </style>
