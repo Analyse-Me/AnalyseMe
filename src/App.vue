09d1655c8c0483674a57ea3f403ea16a893ce40b
Mateusz <mateuszslotwinski16@gmail.com>
2022-01-26 16:01:48 +0100

    I dont need such large scss scripts there

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,86 @@ export default class App extends Vue {}
 
 <style lang="scss">
 @import './styles/index.scss';
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  position: relative;
+}
+
+*::-moz-selection {
+  /* Code for Firefox */
+  background: $main !important;
+}
+
+*::selection {
+  background: $main !important;
+}
+
+html {
+  font-family: 'Rubik', sans-serif !important;
+}
+
+body {
+  padding: 0px !important;
+  margin: 0px !important;
+  overflow-x: hidden;
+  background-color: #ebeaed;
+  min-height: 100vh;
+  color: #262626;
+}
+
+a {
+  text-decoration: none;
+  color: #262626;
+  cursor: pointer;
+}
+
+input,
+button,
+select,
+textarea {
+  font-family: 'Rubik', sans-serif;
+  border: none;
+  background-color: #e3e3e3;
+  color: #333;
+  padding: 0;
+}
+
+button,
+select {
+  cursor: pointer;
+}
+
+button:focus,
+input:focus,
+select:focus,
+textarea:focus {
+  outline: 0 !important;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+
+.katex-html {
+  * {
+    font-family: 'Rubik' !important;
+  }
+}
+
 main {
   display: flex;
   flex-direction: column;
@@ -31,5 +111,12 @@ main {
   .maincontainer {
     flex-grow: 1;
   }
+}
+
+.vue-notification {
+  margin: 0 5px 5px;
+  padding: 10px;
+  font-size: 17px !important;
+  color: theme(#e3e3e3);
 }
 </style>
