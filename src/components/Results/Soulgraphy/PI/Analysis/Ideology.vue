f78d7abffb3ec1da319e502f3b34d9fdf642d72e
Mateusz <mateuszslotwinski16@gmail.com>
2022-03-30 18:16:16 +0200

    ideologies

--- src/components/Results/Soulgraphy/PI/Analysis/Ideology.vue
+++ src/components/Results/Soulgraphy/PI/Analysis/Ideology.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+  <div v-if="ideology">
     <div
       class="title"
       v-html="$t('results.political_ideas.sections.ideology')"
     />
-    <div class="ideology">In progress</div>
-    <div class="description">{{ ideologies }}</div>
+    <div class="ideology">{{ ideology }}</div>
+    <div class="description">{{ ideology }}</div>
   </div>
 </template>
 
@@ -16,17 +16,36 @@ import { Ideology, Result, _Soulgraphy_PI } from '@/data/types/quizzes'
 // import { Prop } from 'vue-property-decorator'
 
 @Options({})
-export default class Null extends Vue {
+export default class Ideologies extends Vue {
   get results(): Result<_Soulgraphy_PI> {
     return this.$store.getters.getResults
   }
   ideologies: Ideology<_Soulgraphy_PI>[] = []
+  ideology = ''
 
   async mounted() {
     const res = await axios.get<Ideology<_Soulgraphy_PI>[]>(
       `/ideologies/${this.results.quizID}`
     )
-    this.ideologies = res.data
+
+    let Ideology: Ideology<_Soulgraphy_PI>
+    let ideologyDist = Infinity
+
+    for (let ideology of res.data) {
+      let dist = 0
+
+      for (let prop in ideology.stats) {
+        if (ideology.stats[prop] == -1) continue
+        dist += Math.abs(ideology.stats[prop] - this.results.results[prop]) ** 2
+      }
+
+      if (dist < ideologyDist) {
+        Ideology = ideology
+        ideologyDist = dist
+      }
+    }
+
+    this.ideology = Ideology!.name
   }
 }
 </script>
